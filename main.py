4585a42fd044b237381b6b092be27b52d1add24b
UnSstrennen <n-i-k-i-t@yandex.ru>
2019-03-23 12:25:30 +0300

    added paths

--- main.py
+++ main.py
@@ -6,5 +6,54 @@ from configparser import ConfigParser
 
 
 config = ConfigParser()
-font = config.get("database", "")
-print(config.read('config.ini'))
+config.read('config.ini')
+print(config.get('connect', 'pas'))
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/idioms')
+def idioms():
+    return render_template('idioms.html')
+
+
+@app.route('/advices')
+def advices():
+    return render_template('advices.html')
+
+
+@app.route('/irregularverbs')
+def irregularverbs():
+    return render_template('irregularverbs.html')
+
+
+@app.route('/motivation')
+def motivation():
+    return render_template('motivation.html')
+
+
+@app.route('/popularwords')
+def popularwords():
+    return render_template('popularwords.html')
+
+
+@app.route('/teachers')
+def teachers():
+    return render_template('teachers.html')
+
+
+@app.route('/timetable')
+def timetable():
+    return render_template('timetable.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
