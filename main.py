1bf8f837db49cdfbb42e1b7c960b10eff61e10f4
UnSstrennen <n-i-k-i-t@yandex.ru>
2019-03-23 19:14:10 +0300

    added login

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 from configparser import ConfigParser
 from transliterate import translit
+from flask_sqlalchemy import SQLAlchemy
 
 
 def to_en(word):
@@ -20,11 +21,39 @@ def to_ru(word, capitalize=False):
 
 config = ConfigParser()
 config.read('config.ini')
-print(config.get('connect', 'pas'))
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + config.get("connect", "DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = config.get("connect", "SECRET_KEY")
+db = SQLAlchemy(app)
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    full_name = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Teacher {} {} {}>'.format(
+            self.id, self.username, self.full_name)
+
+
+class Timetable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(30), unique=True, nullable=False)
+    full_name = db.Column(db.String(80), unique=False, nullable=False)
+    red_week = db.Column(db.String(30), unique=False, nullable=False)
+    green_week = db.Column(db.String(30), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Timetable object>'
+
+
+db.create_all()
 
 
 class LoginForm(FlaskForm):
@@ -39,10 +68,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
-    return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        """ teacher = Teacher(username=form.username.data,
+                          full_name=form.full_name.data,
+                          password=form.password.data
+                          )
+        db.session.add(teacher)
+        db.session.commit() """
+        query = Teacher.query.filter_by(username=form.username.data, password=form.password.data).first()
+        if query is None:
+            return render_template('login.html', form=form, error=True)
+        else:
+            session['username'] = query.username
+            session['user_id'] = query.id
+            return redirect('/index')
+    return render_template('login.html', form=form, error=False)
 
 
 @app.route('/idioms')
